06a30bb61140656e72257bcff9c92097c7b7ba00
Shohel Rana <shohel.rana@bs-23.net>
2021-07-03 12:23:30 +0600

    Authentication code updated

--- lib/network/newHelper.dart
+++ lib/network/newHelper.dart
@@ -1,14 +1,23 @@
+import 'dart:collection';
+
 import 'package:dio/dio.dart';
 import 'package:pageview_with_newsapi/model/news_model.dart';
 
 class NewsHelper {
-  final newsUrl =
-      'https://newsapi.org/v2/top-headlines?country=us&apiKey=2d5e2245aa3e4ea0b856ffdafe43e506';
+  final token = "2d5e2245aa3e4ea0b856ffdafe43e506";
+  final baseUrl = 'https://newsapi.org/v2/top-headlines';
+
+  BaseOptions _baseOption() {
+    Map<String, String> headers = {'Authorization': 'Bearer $token'};
+    Map<String, String> query = {'country': 'us'};
+    return BaseOptions(
+        baseUrl: baseUrl, headers: headers, queryParameters: query);
+  }
 
   Future<News> callNewAPI() async {
     News news;
-    Response response = await Dio().get(
-      newsUrl,
+    Response response = await Dio(_baseOption()).get(
+      baseUrl,
     );
     if (response.statusCode == 200) {
       news = newsFromJson(response.toString());
